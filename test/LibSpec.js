8f9f9807fc98d0ce4e101aca8f67c914bdc00045
Brandon Moser <brandon@brandonmoser.com>
2015-05-15 08:18:12 -0500

    Add some tests

--- test/LibSpec.js
+++ test/LibSpec.js
@@ -1,2 +1,27 @@
-// Tests
-module.exports = {};
+var chai = require('chai')
+  , assert = chai.assert
+  , expect = chai.expect
+  , should = chai.should();
+var lib = require('../lib/index');
+
+describe('Library Functions', function(){
+  this.timeout(5000);
+
+  describe('#callProductsAPI', function(){
+    it('should return an object', function(done){
+      lib.callProductsAPI('13860428', function(err, data){
+        if (err) done(err);
+        data.should.be.an('object');
+        done();
+      });
+    });
+    it('should have property of online_description', function(){
+      lib.callProductsAPI('1', function(err, data){
+        if (err) done(err);
+        expect(data).to.have.any.keys('online_description');
+        done();
+      });
+    });
+
+  });
+});
